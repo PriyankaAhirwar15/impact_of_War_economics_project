d71540a7b7503ca96c67e3bf4f07fa8eb60fb30f
Priyanka <priyanka15.india@gmail.com>
2026-04-01 22:24:03 +0530

    Initial upload of clean project files

--- utils.py
+++ utils.py
@@ -2,8 +2,13 @@ import os
 import pandas as pd
 import streamlit as st
 
-@st.cache_data
+@st.cache_data # This saves memory and speeds up the app
 def load_data():
-    base = os.path.dirname(os.path.abspath(__file__))
-    path = os.path.join(base, 'war_economic_impact_dataset.csv')
-    return pd.read_csv(path)
+    # This finds the folder where utils.py is sitting
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    # This points to the dataset inside the 'War_Economic_Project' folder
+    # Note: If your CSV is in the main folder, remove 'dataset/'
+    csv_path = os.path.join(base_dir, 'war_economic_impact_dataset.csv')
+    
+    return pd.read_csv(csv_path)
